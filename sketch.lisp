d82c4fef28f997c4d1e7f7bf62a46967a19559d2
Ömercan Balandı <balandiomer@gmail.com>
2020-12-25 02:47:10 +0300

    symbols and lambda expressions

--- sketch.lisp
+++ sketch.lisp
@@ -74,6 +74,7 @@
       (z (+ x 4)))
   (print-line (multiply (list x y z))))
 
+; lexical -> static
 ; Dynamic variables are sort of like global variables, but more useful: they are dynamically scoped. You define them either with defvar or defparameter, the differences being:
 ;   defparameter requires an initial value, defvar does not.
 ;   defparameter variables are changed when code is reloaded with a new initial value, defvar variables are not.
@@ -107,3 +108,34 @@
 (defparameter numbers (list 1 2 3 4))
 (setf (nth 2 numbers) 80)
 (print-line numbers)
+
+; https://lispmethods.com/symbols.html
+; Essentially, symbols are objects, and, to 
+; access them, you either need a reference to them in memory, 
+; or you need to know their string name and “home package”.
+
+; (setq bla 10) is roughly equivalent to (set 'bla 10), and can be 
+; thought of as “set quoted”. It sets the value of the symbol bla, 
+; rather than the value of the symbol stored in bla. This is an 
+; oversimplification, as SET won’t work on lexically scoped variables, 
+; while SETQ will. Have a look at their CLHS pages for more precise detail.
+
+
+; https://stackoverflow.com/a/13213772/13615958
+; LAMBDA is a macro. It expands (lambda ...) to (function (lambda ...)), 
+; which is the equivalent of #'(lambda ...)).
+; 
+; The macro saves you a bit of writing/reading, that's all. In the first 
+; version of Common Lisp (CLtL1) there was no LAMBDA macro. It has been added 
+; later and is now a part of ANSI Common Lisp,
+
+; The FUNCTION special operator
+
+; FUNCTION is a special operator. It expects a function name or a lambda 
+; expression. Thus the name or the lambda expression are not evaluated. 
+; In fact lambda expressions can't be evaluated at all. Inside FUNCTION, 
+; the lambda expression is not a macro form and thus will not be expanded again. 
+; The purpose of FUNCTION is to return the corresponding function object 
+; which is denoted by the name or by the lambda expression. It returns 
+; the function object as a value. With this special operator one can access 
+; the function object from global functions and lexical functions.
